e45915ad05257826193e847428c45d24764be7da
khushal <chandakk008@gmail.com>
2024-07-17 23:31:43 +0530

    First Commit

---
+++ src/main/java/com/example/Ecom_project/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.Ecom_project.controller;
+
+import com.example.Ecom_project.model.Product;
+import com.example.Ecom_project.repo.ProductRepo;
+import com.example.Ecom_project.service.ProductServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+@CrossOrigin
+public class ProductController {
+    @Autowired
+    ProductServices services;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return services.getAllProducts();
+    }
+
+}
---
+++ src/main/java/com/example/Ecom_project/model/Product.java
@@ -0,0 +1,29 @@
+package com.example.Ecom_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name, desc, brand,category;
+    private BigInteger price;
+    private Date releaseDate;
+    private boolean available;
+    private int stock;
+}
